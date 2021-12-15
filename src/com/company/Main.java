a63fe93d884787e409937cca75af9d1215759ac9
blzimme000 <blzimme000@birdvilleschools.net>
2021-12-15 15:22:26 -0600

    Made it into a seperate method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,12 +11,18 @@ public class Main {
         System.out.println("Input an integer to be factored:");
         System.out.println("Note: Must be positive. If negative, remove negative and make one factor negative");
         double a = scan.nextDouble();
+        System.out.println(factor(a));
+    }
+
+    public static String factor(double a){
+        String thingtoreturn = "";
         for (int i = 1; i < (.5*a); i++) {
             if (i <= a / i) {
                 if (a % i == 0) {
-                    System.out.println("" + i + ", " + (a / i) + ". Sum: " + (i + (a / i)) + ". Difference: " + ((a / i) - i));
+                    thingtoreturn += "" + i + ", " + (a / i) + ". Sum: " + (i + (a / i)) + ". Difference: " + ((a / i) - i) + "\n";
                 }
             }
         }
+        return thingtoreturn;
     }
 }
