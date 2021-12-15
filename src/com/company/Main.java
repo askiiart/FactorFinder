6c5a762128abe64356e5997f73d5cd2d828891da
blzimme000 <blzimme000@birdvilleschools.net>
2021-12-15 15:14:25 -0600

    Fixed difference/duplicates bug.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,22 +4,17 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Input the number:");
+        System.out.println("Input an integer to be factored:");
         System.out.println("Note: Must be positive. If negative, remove negative and make one factor negative");
         double a = scan.nextDouble();
-
-        for (int i = 1; i < (.5*a); i++){
-            if (a%i == 0){
-                System.out.print("" + i + ", " + (a/i) + ". Sum: " + (i + (a/i)));
-                //Adds difference (might not work, but not sure if that's because of bad online compiler)
-                if (i < a%i){
-                    System.out.println(". Difference: " + (i - (a/i)));
-                } else {
-                    System.out.println(". Difference: " + ((a/i) - i));
+        for (int i = 1; i < (.5*a); i++) {
+            if (i <= a / i) {
+                if (a % i == 0) {
+                    System.out.println("" + i + ", " + (a / i) + ". Sum: " + (i + (a / i)) + ". Difference: " + ((a / i) - i));
                 }
             }
         }
